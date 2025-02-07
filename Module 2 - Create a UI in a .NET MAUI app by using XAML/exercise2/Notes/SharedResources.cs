9a9b0e614407ae806626c54d7d070d0a3af06d22
Chris Nightingale <wooffet@gmail.com>
2025-02-07 15:13:13 +0000

    Add SharedResources class, use shared resources to control font and bg color of ui elements, change height of editor element based on platform

---
+++ Module 2 - Create a UI in a .NET MAUI app by using XAML/exercise2/Notes/SharedResources.cs
@@ -0,0 +1,8 @@
+﻿namespace Notes
+{
+    static class SharedResources
+    {
+        public static readonly Color FontColor = Color.FromRgb(0, 0, 0xFF);
+        public static readonly Color BackgroundColor = Color.FromRgb(0xFF, 0xF0, 0xAD);
+    }
+}
