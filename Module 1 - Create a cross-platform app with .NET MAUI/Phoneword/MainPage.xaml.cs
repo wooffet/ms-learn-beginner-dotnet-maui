ad83df3939655b703628ce82e3a639e8ee521ac6
Chris Nightingale <wooffet@gmail.com>
2025-02-07 11:27:51 +0000

    Remove template code in MainPage code-behind

--- Module 1 - Create a cross-platform app with .NET MAUI/Phoneword/MainPage.xaml.cs
+++ Module 1 - Create a cross-platform app with .NET MAUI/Phoneword/MainPage.xaml.cs
@@ -1,25 +1,9 @@
-﻿namespace Phoneword
+﻿namespace Phoneword;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    public MainPage()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count += 5;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+        InitializeComponent();
     }
-
 }
